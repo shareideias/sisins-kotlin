f9fbe37c9dd51453852e3370afdc63af1e951e31
adriantodt <adrian.todtsilva@gmail.com>
2019-12-03 16:55:57 -0300

    Uso do template

--- src/main/kotlin/br/com/associacaoshare/view/alunos/ListaView.kt
+++ src/main/kotlin/br/com/associacaoshare/view/alunos/ListaView.kt
@@ -1,31 +1,13 @@
 package br.com.associacaoshare.view.alunos
 
-import br.com.associacaoshare.view.base.HtmlBuilderView
+import br.com.associacaoshare.view.base.SisInsAlunoView
 import io.javalin.http.Context
 import kotlinx.html.*
 
-class ListaView : HtmlBuilderView() {
-    override fun HTML.render(ctx: Context) {
-        head {
-            title("Inscrição Share")
+class ListaView : SisInsAlunoView() {
+    override val pageTitle = "Lista de Inscrições"
 
-            link(rel = "icon", href = "/img/globo.png")
-            meta(charset = "utf-8")
-
-            link("https://fonts.googleapis.com/icon?family=Material+Icons", rel = "stylesheet")
-            link(type = "text/css", rel = "stylesheet", href = "/css/materialize.min.css")
-            link(type = "text/css", rel = "stylesheet", href = "../css/lista.css")
-
-            meta("viewport", "width=device-width, initial-scale=1.0")
-        }
-
-        body {
-            main { main() }
-            scripts()
-        }
-    }
-
-    private fun MAIN.main() {
+    override fun BODY.renderBody(ctx: Context) {
         div("row") {
             div("col s12 m0 l3")
             div("col s12 m12 l3") {
@@ -326,10 +308,4 @@ class ListaView : HtmlBuilderView() {
         }
         div("col s12 m0 l3")
     }
-
-    //JavaScript para animação e validação de email e senha
-    private fun BODY.scripts() {
-        script("text/javascript", "../js/materialize.min.js")
-    }
-
 }
