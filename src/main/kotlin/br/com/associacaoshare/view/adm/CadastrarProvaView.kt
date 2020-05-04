2d4bb6f6ed61c3817a1d4f77cbbdefc931d92252
Gabriela Bergamo <gabiibergamo@gmail.com>
2020-05-04 18:59:05 -0300

    Atualizacao da view CadastrarProvaView

--- src/main/kotlin/br/com/associacaoshare/view/adm/CadastrarProvaView.kt
+++ src/main/kotlin/br/com/associacaoshare/view/adm/CadastrarProvaView.kt
@@ -12,16 +12,345 @@ class CadastrarProvaView : SisInsAdmView() {
          +"Criação da Prova de nivelamento"}
 
 
-        div(classes = "row") {
+        div(classes = "row rowzeras") {
+            //Pergunta 1
+            h4{
+                +"Pergunta 1"
+            }
             div(classes = "input-field col s12") {
                 input(classes = "validate") {
                     id = "first_name"
                 }
                 label {
                     htmlFor = "first_name"
-                    +"Pergunta 1"
+                    +"Pergunta"
                 }
             }
+
+            div(classes = "input-field col s12") {
+                input(classes = "validate") {
+                    id = "first_name"
+                }
+                label {
+                    htmlFor = "first_name"
+                    +"Alternativa 1"
+                }
+            }
+
+            div(classes = "input-field col s12") {
+                input(classes = "validate") {
+                    id = "first_name"
+                }
+                label {
+                    htmlFor = "first_name"
+                    +"Alternativa 2"
+                }
+            }
+
+            div(classes = "input-field col s12") {
+                input(classes = "validate") {
+                    id = "first_name"
+                }
+                label {
+                    htmlFor = "first_name"
+                    +"Alternativa 3"
+                }
+            }
+
+            div(classes = "input-field col s12 ultima_alt") {
+                input(classes = "validate") {
+                    id = "first_name"
+                }
+                label {
+                    htmlFor = "first_name"
+                    +"Alternativa 4"
+                }
+            }
+            //fim Pergunta 1
+
+            //Pergunta 2
+            h4{
+                +"Pergunta 2"
+            }
+            div(classes = "input-field col s12") {
+                input(classes = "validate") {
+                    id = "first_name"
+                }
+                label {
+                    htmlFor = "first_name"
+                    +"Pergunta"
+                }
+            }
+
+            div(classes = "input-field col s12") {
+                input(classes = "validate") {
+                    id = "first_name"
+                }
+                label {
+                    htmlFor = "first_name"
+                    +"Alternativa 1"
+                }
+            }
+
+            div(classes = "input-field col s12") {
+                input(classes = "validate") {
+                    id = "first_name"
+                }
+                label {
+                    htmlFor = "first_name"
+                    +"Alternativa 2"
+                }
+            }
+
+            div(classes = "input-field col s12") {
+                input(classes = "validate") {
+                    id = "first_name"
+                }
+                label {
+                    htmlFor = "first_name"
+                    +"Alternativa 3"
+                }
+            }
+
+            div(classes = "input-field col s12 ultima_alt") {
+                input(classes = "validate") {
+                    id = "first_name"
+                }
+                label {
+                    htmlFor = "first_name"
+                    +"Alternativa 4"
+                }
+            }
+            //fim Pergunta 2
+
+            //Pergunta 3
+            h4{
+                +"Pergunta 3"
+            }
+            div(classes = "input-field col s12") {
+                input(classes = "validate") {
+                    id = "first_name"
+                }
+                label {
+                    htmlFor = "first_name"
+                    +"Pergunta"
+                }
+            }
+
+            div(classes = "input-field col s12") {
+                input(classes = "validate") {
+                    id = "first_name"
+                }
+                label {
+                    htmlFor = "first_name"
+                    +"Alternativa 1"
+                }
+            }
+
+            div(classes = "input-field col s12") {
+                input(classes = "validate") {
+                    id = "first_name"
+                }
+                label {
+                    htmlFor = "first_name"
+                    +"Alternativa 2"
+                }
+            }
+
+            div(classes = "input-field col s12") {
+                input(classes = "validate") {
+                    id = "first_name"
+                }
+                label {
+                    htmlFor = "first_name"
+                    +"Alternativa 3"
+                }
+            }
+
+            div(classes = "input-field col s12 ultima_alt") {
+                input(classes = "validate") {
+                    id = "first_name"
+                }
+                label {
+                    htmlFor = "first_name"
+                    +"Alternativa 4"
+                }
+            }
+            //fim Pergunta 3
+
+            //Pergunta 4
+            h4{
+                +"Pergunta 4"
+            }
+            div(classes = "input-field col s12") {
+                input(classes = "validate") {
+                    id = "first_name"
+                }
+                label {
+                    htmlFor = "first_name"
+                    +"Pergunta"
+                }
+            }
+
+            div(classes = "input-field col s12") {
+                input(classes = "validate") {
+                    id = "first_name"
+                }
+                label {
+                    htmlFor = "first_name"
+                    +"Alternativa 1"
+                }
+            }
+
+            div(classes = "input-field col s12") {
+                input(classes = "validate") {
+                    id = "first_name"
+                }
+                label {
+                    htmlFor = "first_name"
+                    +"Alternativa 2"
+                }
+            }
+
+            div(classes = "input-field col s12") {
+                input(classes = "validate") {
+                    id = "first_name"
+                }
+                label {
+                    htmlFor = "first_name"
+                    +"Alternativa 3"
+                }
+            }
+
+            div(classes = "input-field col s12 ultima_alt") {
+                input(classes = "validate") {
+                    id = "first_name"
+                }
+                label {
+                    htmlFor = "first_name"
+                    +"Alternativa 4"
+                }
+            }
+            //fim Pergunta 4
+
+            //Pergunta 5
+            h4{
+                +"Pergunta 5"
+            }
+            div(classes = "input-field col s12") {
+                input(classes = "validate") {
+                    id = "first_name"
+                }
+                label {
+                    htmlFor = "first_name"
+                    +"Pergunta"
+                }
+            }
+
+            div(classes = "input-field col s12") {
+                input(classes = "validate") {
+                    id = "first_name"
+                }
+                label {
+                    htmlFor = "first_name"
+                    +"Alternativa 1"
+                }
+            }
+
+            div(classes = "input-field col s12") {
+                input(classes = "validate") {
+                    id = "first_name"
+                }
+                label {
+                    htmlFor = "first_name"
+                    +"Alternativa 2"
+                }
+            }
+
+            div(classes = "input-field col s12") {
+                input(classes = "validate") {
+                    id = "first_name"
+                }
+                label {
+                    htmlFor = "first_name"
+                    +"Alternativa 3"
+                }
+            }
+
+            div(classes = "input-field col s12 ultima_alt") {
+                input(classes = "validate") {
+                    id = "first_name"
+                }
+                label {
+                    htmlFor = "first_name"
+                    +"Alternativa 4"
+                }
+            }
+            //fim Pergunta 5
+
+            //Pergunta 6
+            h4{
+                +"Pergunta 6"
+            }
+            div(classes = "input-field col s12") {
+                input(classes = "validate") {
+                    id = "first_name"
+                }
+                label {
+                    htmlFor = "first_name"
+                    +"Pergunta"
+                }
+            }
+
+            div(classes = "input-field col s12") {
+                input(classes = "validate") {
+                    id = "first_name"
+                }
+                label {
+                    htmlFor = "first_name"
+                    +"Alternativa 1"
+                }
+            }
+
+            div(classes = "input-field col s12") {
+                input(classes = "validate") {
+                    id = "first_name"
+                }
+                label {
+                    htmlFor = "first_name"
+                    +"Alternativa 2"
+                }
+            }
+
+            div(classes = "input-field col s12") {
+                input(classes = "validate") {
+                    id = "first_name"
+                }
+                label {
+                    htmlFor = "first_name"
+                    +"Alternativa 3"
+                }
+            }
+
+            div(classes = "input-field col s12 ultima_alt") {
+                input(classes = "validate") {
+                    id = "first_name"
+                }
+                label {
+                    htmlFor = "first_name"
+                    +"Alternativa 4"
+                }
+            }
+            //fim Pergunta 6
+
+
+            //botao
+            div(classes = "row") {
+                a("#", classes = "entrar waves-effect waves-light btn") {
+                    +"Cadastrar"
+                }
+            }
+            //fim botao
         }
 
 
