5f2cb38c85dd27ee76a1b732a2de3319be5e8b7a
Vinícius <vinicius_aiala@outlook.com>
2020-05-15 20:28:45 -0300

    Reestruturação do controller para controle de acesso

--- src/main/kotlin/br/com/associacaoshare/controller/AlunosController.kt
+++ src/main/kotlin/br/com/associacaoshare/controller/AlunosController.kt
@@ -1,6 +1,6 @@
 package br.com.associacaoshare.controller
 
-import br.com.associacaoshare.model.Participante
+import br.com.associacaoshare.controller.SisinsAccessManager.Roles.*
 import br.com.associacaoshare.model.dao.DataAccessObject
 import br.com.associacaoshare.model.exception.FalhaSessaoException
 import br.com.associacaoshare.view.alunos.*
@@ -9,92 +9,71 @@ import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
 import io.javalin.apibuilder.ApiBuilder.get
 import io.javalin.apibuilder.ApiBuilder.post
+import io.javalin.core.security.SecurityUtil.roles
 import io.javalin.http.Context
 import org.kodein.di.generic.instance
+import java.net.URLDecoder.decode
+import kotlin.text.Charsets.UTF_8
 
 class AlunosController (override val kodein: Kodein) : EndpointGroup, KodeinAware {
     val dao: DataAccessObject by instance()
 
     override fun addEndpoints() {
-        get(::index)
+        get("editar", ::edicao, roles(PARTICIPANTE))
+        post("EditaProc", ::edicaoProc, roles(PARTICIPANTE))
 
-        get("cadastro", ::cadastro)
-        post("CadastroProc", ::cadastroProc)
+        get(::inscricoes, roles(PARTICIPANTE))
 
-        get("login", ::login)
-
-        get("editar", ::edicao)
-        post("EditaProc", ::edicaoProc)
-
-        get("inscricoes", ::inscricoes)
-
-        get("ListaView", ListaView()::render)
-        get("ProvaView", ProvaView()::render)
-    }
-
-    private fun index (ctx: Context) {
-        IndexView().render(ctx)
-    }
-
-    private fun cadastro (ctx: Context) {
-        var errormsg = dao.asciitouni(ctx.cookie("errorMsg"))
-        if(errormsg != null)
-            ctx.cookie("errorMsg", "", 0)
-        CadastroView(errormsg).render(ctx)
-    }
-
-    private fun cadastroProc (ctx: Context) {
-        val resp = ctx.formParamMap()
-        val novoParticipante: Participante = dao.insertParticipante(resp)
-        ctx.redirect("/alunos/login")
-    }
-
-    private fun login (ctx: Context) {
-        LoginView().render(ctx)
+        get("ListaView", ::lista, roles(PARTICIPANTE))
+        get("ProvaView", ::prova, roles(PARTICIPANTE))
     }
 
     private fun edicao (ctx: Context) {
-        /* TODO: Alterar o redirect baseado no determinado.
-        *        Alterar a obtenção do id do usuario pela implementação do login.
-         */
-        val participante = ctx.sessionAttribute<Int?>("id_usuario")?.let { dao.getParticipante(it) }
-        val errormsg = dao.asciitouni(ctx.cookie("errorMsg"))
+        val participante = ctx.sessionAttribute<Int?>("ID")?.let { dao.getParticipante(it) }
+        val errormsg = ctx.cookie("errorMsg")?.let{decode(it , UTF_8)}
         if (errormsg != null)
             ctx.cookie("errorMsg", "", 0)
         if(participante != null) {
             EdicaoView(errormsg, participante).render(ctx)
         } else {
             throw FalhaSessaoException()
-            ctx.redirect("/alunos/login")
         }
     }
 
     private fun edicaoProc (ctx: Context) {
-        /* TODO: Alterar o redirect baseado no determinado.
-        *        Alterar a obtenção do id do usuario pela implementação do login.
-         */
         val resp = ctx.formParamMap()
-        val participante = ctx.sessionAttribute<Int?>("id_usuario")?.let { dao.getParticipante(it) }
+        val participante = ctx.sessionAttribute<Int?>("ID")?.let { dao.getParticipante(it) }
         if(participante != null) {
             participante.atualizaDados(resp)
             dao.updateParticipante(participante)
         }
-        //Onde redirecionar após edição?
         ctx.redirect("/alunos")
     }
 
     private fun inscricoes (ctx: Context) {
-        /* TODO: Alterar o redirect baseado no determinado.
-        *        Alterar a obtenção do id do usuario pela implementação do login.
-         */
-        val participante = ctx.sessionAttribute<Int?>("id_usuario")?.let { dao.getParticipante(it) }
-        val errormsg = dao.asciitouni(ctx.cookie("errorMsg"))
+        val participante = ctx.sessionAttribute<Int?>("ID")?.let { dao.getParticipante(it) }
+        val errormsg = ctx.cookie("errorMsg")?.let{decode(it , UTF_8)}
         if (errormsg != null)
             ctx.cookie("errorMsg", "", 0)
+        println(participante)
         if(participante != null) {
             InscricoesAlunoView(errormsg).render(ctx)
         } else {
             throw FalhaSessaoException()
         }
+    }
+
+    private fun lista (ctx: Context) {
+        val errormsg = ctx.cookie("errorMsg")?.let{decode(it , UTF_8)}
+        if (errormsg != null)
+            ctx.cookie("errorMsg", "", 0)
+        ListaView(errormsg)
+    }
+
+    private fun prova (ctx: Context) {
+        val errormsg = ctx.cookie("errorMsg")?.let{decode(it , UTF_8)}
+        if (errormsg != null)
+            ctx.cookie("errorMsg", "", 0)
+        ProvaView(errormsg)
     }
 }
